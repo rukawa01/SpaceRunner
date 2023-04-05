3096a07960c79bc92ba0e5a225e8d97b31ca0778
Ömer Güven <omer7539512486@gmail.com>
2023-04-05 13:34:05 +0300

    First Day

---
+++ Assets/Kodlar/MoveCharacter.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A ) && gameObject.name == "Character")
+        {
+            transform.position += new Vector3(-10,0,0) * Time.deltaTime ;
+        }
+        if (Input.GetKey(KeyCode.D) && gameObject.name == "Character")
+        {
+            transform.position += new Vector3(10, 0,0) * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.W) && gameObject.name== "Main Camera") 
+        { transform.position += new Vector3(0, 0, 10) * Time.deltaTime; }
+
+        if (Input.GetKey(KeyCode.S) && gameObject.name == "Main Camera")
+        { transform.position += new Vector3(0, 0, -10) * Time.deltaTime; }
+    }
+}
